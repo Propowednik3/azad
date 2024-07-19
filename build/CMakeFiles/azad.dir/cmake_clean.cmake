f2a0478efc96015b7be3f7731ad2efb18aad19b1
Propowednik3 <propowednik3@gmail.com>
2024-07-19 15:31:50 +0400

    CMake add dir build

---
+++ build/CMakeFiles/azad.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/azad.dir/src/audio.c.o"
+  "CMakeFiles/azad.dir/src/bcm2835.c.o"
+  "CMakeFiles/azad.dir/src/debug.c.o"
+  "CMakeFiles/azad.dir/src/dummy.c.o"
+  "CMakeFiles/azad.dir/src/flv-demuxer.c.o"
+  "CMakeFiles/azad.dir/src/flv-muxer.c.o"
+  "CMakeFiles/azad.dir/src/globals.c.o"
+  "CMakeFiles/azad.dir/src/gpio.c.o"
+  "CMakeFiles/azad.dir/src/ir_control.c.o"
+  "CMakeFiles/azad.dir/src/main.c.o"
+  "CMakeFiles/azad.dir/src/nal_to_rtp.c.o"
+  "CMakeFiles/azad.dir/src/network.c.o"
+  "CMakeFiles/azad.dir/src/nfc.c.o"
+  "CMakeFiles/azad.dir/src/omx_client.c.o"
+  "CMakeFiles/azad.dir/src/onvif.c.o"
+  "CMakeFiles/azad.dir/src/pthread2threadx.c.o"
+  "CMakeFiles/azad.dir/src/rc522.c.o"
+  "CMakeFiles/azad.dir/src/rtsp.c.o"
+  "CMakeFiles/azad.dir/src/streamer.c.o"
+  "CMakeFiles/azad.dir/src/system.c.o"
+  "CMakeFiles/azad.dir/src/text_func.c.o"
+  "CMakeFiles/azad.dir/src/tfp625a.c.o"
+  "CMakeFiles/azad.dir/src/weather.c.o"
+  "CMakeFiles/azad.dir/src/weather_func.c.o"
+  "CMakeFiles/azad.dir/src/web.c.o"
+  "CMakeFiles/azad.dir/src/widgets.c.o"
+  "CMakeFiles/azad.dir/src/writer.c.o"
+  "azad.pdb"
+  "azad"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/azad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
