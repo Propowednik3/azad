a94dbc361a6b8cc913b2dc502d42011f0e299495
Azat <ed-209@mail.ru>
2025-01-10 12:19:31 +0400

    inc version

--- src/version.h
+++ src/version.h
@@ -4,10 +4,10 @@
 #define azad_VERSION_MAJOR 10
 #define azad_VERSION_MINOR 0
 #define azad_VERSION_PATCH 0
-#define azad_VERSION_BUILD 1165
+#define azad_VERSION_BUILD 1166
 #define azad_VERSION_META ""
-#define azad_VERSION "10.1165.20241228.143234"
-#define azad_COMPILE_DATE "20241228"
-#define azad_COMPILE_TIME "143234"
+#define azad_VERSION "10.1166.20250110.121803"
+#define azad_COMPILE_DATE "20250110"
+#define azad_COMPILE_TIME "121803"
 
 #endif /* azad_VERSION_H */
